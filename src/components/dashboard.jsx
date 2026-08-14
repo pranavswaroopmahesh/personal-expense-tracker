ec2d2811a4e7c00e32440d1b481b77b20fe94a1f
pranavswaroopmahesh <pranavaswaroopmahesh24cs@rnsit.ac.in>
2026-08-14 17:34:39 +0530

    added dashbaord and cards

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,9 +1,9 @@
 import Card from "./Card";
 import { useState } from "react";
-import Card from "./Card";
 
 function Dashboard() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+
   return (
     <main className="dashboard">
       <h2>Welcome Back! 👋</h2>
@@ -14,14 +14,15 @@ function Dashboard() {
         <Card title="Expenses" amount="₹0" />
         <Card title="Savings" amount="₹0" />
       </div>
+
       <h3>Counter: {count}</h3>
 
-<button
-  className="add-btn"
-  onClick={() => setCount(count + 1)}
->
-  Click Me
-</button>
+      <button
+        className="add-btn"
+        onClick={() => setCount(count + 1)}
+      >
+        Click Me
+      </button>
 
       <button className="add-btn">
         + Add Expense
